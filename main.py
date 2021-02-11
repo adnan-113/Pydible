3a532de1d5d5a7ed1d237eba049fd3f15fc67129
Namz <namz@namz.gq>
2021-02-11 17:30:23 +0530

    fixed some errors, still working on it

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
- import pyttsx3
- import PyPDF2
- book = open('book.pdf', 'rb')
- pdfReader = PyPDF2.pdfFileReader(book)
- pages = pdfReader.numPages
- print (pages)
- speaker = pyttsx3.init()
- for num in range(1, pages)
-   page = pdfReader.getpage(1)
-   text = page.extractText()
-   speaker.say(text)
-   speaker.runAndWait()
+import pyttsx3
+import PyPDF2
+
+# Get the name of the book 
+book_name= input("Enter the name of the pdf: ")
+
+# Open the book
+book = open(book_name, 'rb')
+
+# read the book
+pdfReader = PyPDF2.PdfFileReader(book)
+
+# get the number of pages
+pages = pdfReader.numPages
+
+print(pages)
+
+
+# # initlialize the pyttsx3 module
+# speaker = pyttsx3.init()
+
+for num in range(1, pages):
+  page = pdfReader.getPage(1)
+  text = page.extractText()
+  print(text)
+  # speaker.say(text)
+  # speaker.runAndWait()
